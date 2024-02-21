898dc5c623d2092b3686895b060e2d5a66ab7dd8
Atanas97 <atanas19.97@abv.bg>
2024-02-21 17:23:27 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">
+                React ready boilerplate with TailwindCSS!
+            </h1>
+        </>
+    );
+}
+
+export default App;
